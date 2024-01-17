d2a07731b16677b2ce6612a5eed7aaa4b77b2331
daivd_abu <david@livly.com>
2024-01-17 17:15:56 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
-git submodule init
-git submodule update
+git submodule update --init --recursive --remote 
 echo "finish submodule init"
+filenames=('./server/shared/database/models/UploadMultiPart.model.js')
+
+for filename in ${filenames[@]}; do
+    if [ -f $filename ]; then
+        echo "$filename exists."
+    else
+        echo "$filename does not exist."
+    fi
+done
 
 npm install
