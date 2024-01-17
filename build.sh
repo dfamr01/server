61f1d86be0901667648fd40c147d52e9df2adca1
daivd_abu <david@livly.com>
2024-01-17 17:34:06 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
+
+# Start the SSH agent and add the private key
+eval $(ssh-agent -s)
+echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add -
+
+# Setup GitHub as known host
+mkdir -p ~/.ssh
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
 git submodule update --init --recursive --remote 
 echo "finish submodule init"
 filenames=('./server/shared/database/models/UploadMultiPart.model.js')
